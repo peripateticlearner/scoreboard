d12c836f077c944c3da2fc2671ae6be6ecb4ca6e
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2024-10-08 16:09:30 -0700

    optimized to be mobile first and added minus points function

--- index.js
+++ index.js
@@ -28,6 +28,28 @@ function plusThreeHome() {
     updateHomeScore()
 }
 
+// Functions to remove points for Home team
+function minusOneHome() {
+    if (scoreHome > 0) {
+        scoreHome -= 1;
+        updateHomeScore();
+    }
+}
+
+function minusTwoHome() {
+    if (scoreHome >= 2) {
+        scoreHome -= 2;
+        updateHomeScore();
+    }
+}
+
+function minusThreeHome() {
+    if (scoreHome >= 3) {
+        scoreHome -= 3;
+        updateHomeScore();
+    }
+}
+
 // Functions for Guest team
 function plusOneGuest() {
     scoreGuest += 1
@@ -42,4 +64,26 @@ function plusTwoGuest() {
 function plusThreeGuest() {
     scoreGuest += 3
     updateGuestScore()
+}
+
+// Functions to remove points for Guest team
+function minusOneGuest() {
+    if (scoreGuest > 0) {
+        scoreGuest -= 1;
+        updateGuestScore();
+    }
+}
+
+function minusTwoGuest() {
+    if (scoreGuest >= 2) {
+        scoreGuest -= 2;
+        updateGuestScore();
+    }
+}
+
+function minusThreeGuest() {
+    if (scoreGuest >= 3) {
+        scoreGuest -= 3;
+        updateGuestScore();
+    }
 }
